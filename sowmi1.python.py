fd72a42e76fa4e69320b68b50350d017cf728682
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 14:27:41 +0530

    level1-python1

---
+++ sowmi1.python.py
@@ -0,0 +1,15 @@
+print("ArithMetic Operation")
+print("----------------------------")
+a=int(input("Enter the First Number:"))
+b=int(input("Enter The Second Number:"))
+print("Result")
+c=a+b
+print("Add:",c)
+c=a-b
+print("Sub:",c)
+c=a*b
+print("Mul:",c)
+c=a/b
+print("div:",c)
+c=a%b
+print("Mod:",c)
