6bdeede228b5cc0cff3089101d57d2880cf19a3d
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 14:40:38 +0530

    level2-program1

---
+++ sowmi21.python.py
@@ -0,0 +1,8 @@
+print("circle program")
+print("----------------")
+r=int(input("Enter The r Value:"))
+print("Result")
+pericir=2*22/7*r
+print("perimeter of cicle:",pericir)
+areacir=22/7*r*r
+print("area of circle:",areacir)
