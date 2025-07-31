2071a59a2e27a819c38d7b9997cb922ab311e2c1
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 14:58:37 +0530

    level2-program2

---
+++ 2sowmi2.python.py
@@ -0,0 +1,14 @@
+print("Square And Rectangle")
+print("-----------------------")
+a=int(input("Enter The A Value:"))
+b=int(input("Enter The B Value:"))
+l=int(input("Enter The L Value:"))
+print("Result:")
+ps=4*a
+print("Perimeter Of Square:",ps)
+As=2*(l+b)
+print("Area Of Square:",As)
+pr=2*(l+b)
+print("Perimeter Of Rectangle:",pr)
+ar=l*b
+print("Area Of Rectangle:",ar)
