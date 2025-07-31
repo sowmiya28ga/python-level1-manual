404b99548375c90ec81d6448feeccbf11c21e692
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 14:45:35 +0530

    level2-program3

---
+++ sowmi23.python.py
@@ -0,0 +1,11 @@
+print("Triangle Program")
+print("---------------------------")
+a=int(input("Enter The A Value:"))
+b=int(input("Enter The B Value:"))
+c=int(input("Enter The C Value:"))
+h=int(input("Enter The H Value:"))
+print("Result")
+pt=a+b+c
+print("Perimter Of The Triangle:",pt)
+at=1/2*b*h
+print("Area Of Triangle:",at)
