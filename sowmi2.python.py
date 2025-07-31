9538219b4a82e8414f92e489c0756401e3faa39b
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 14:30:17 +0530

    level1-program2

---
+++ sowmi2.python.py
@@ -0,0 +1,14 @@
+print("Takshashila University")
+print("Ongur,Tindivanam,Villupuram Dist.TN")
+print("------------------------------------")
+print("Student Mark List")
+print("------------------------------------")
+str=input("Enter The Register No:")
+str=input("Enter The Name:")
+pymark=int(input("Python Mark:"))
+dbmark=int(input("Dbms Mark:"))
+acmark=int(input("Accounting Mark:"))
+total=pymark+dbmark+acmark
+print("Total Mark:",total)
+avg=total/3
+print("Average mark:",avg)
