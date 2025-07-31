fbf759b12cbfc31bc9fd24b509aa917dfbfb8f4d
sowmiya28ga <gasowmiya28@gmail.com>
2025-07-31 15:08:06 +0530

    level2-program5

---
+++ sowmi25.python.py
@@ -0,0 +1,14 @@
+print("Exponential Program")
+print("-----------------------------")
+x=int(input("Enter The X Value:"))
+y=int(input("Enter The Y Value:"))
+z=int(input("enter The Z Value"))
+print("Result")
+p=x^2+y^2+z^2
+print("P:",p)
+q=x^3+y^2+z+20
+print("Q:",q)
+r=x^2*y+y^2*z+z^2*x+10
+print("R:",r)
+s=x^3*y^2*z+2*x*y*z+x*y*z
+print("S:",s)
